77cc8dca3b728a425984d8ed678ed5364a3bd4c8
Dei-Filius <hwdkong@hotmail.sg>
2023-03-01 17:18:40 +0800

    incremental changes to login.html and login.js

---
+++ public/js/login.js
@@ -0,0 +1,10 @@
+const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be in the authorized domains list in the Firebase Console.
+    url: "https://localhost:5000/",
+    
+    // This must be true.
+    handleCodeInApp: true
+};
+
+
