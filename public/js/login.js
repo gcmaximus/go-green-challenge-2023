e13e48492d2341a508fac23314eb4093ec7e0781
Dei-Filius <hwdkong@hotmail.sg>
2023-03-01 23:32:45 +0800

    lazy write

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,6 @@ var capital = document.getElementById("capital");
 var number = document.getElementById("number");
 var length = document.getElementById("length");
 
-
 // When the user starts to type something inside the password field
 myInput.onkeyup = function () {
     // Validate lowercase letters
@@ -46,104 +45,129 @@ myInput.onkeyup = function () {
         length.classList.remove("valid");
         length.classList.add("invalid");
     }
-}
+};
 
 // Form stuff
-const toggleForm = document.querySelector('.toggle-form');
-const loginForm = document.querySelector('#login-form');
-const signupForm = document.querySelector('#signup-form');
-const cardToggleBtn = document.querySelector('.card-toggle button');
+const toggleForm = document.querySelector(".toggle-form");
+const loginForm = document.querySelector("#login-form");
+const signupForm = document.querySelector("#signup-form");
+const cardToggleBtn = document.querySelector(".card-toggle button");
 
 // Toggle between login and signup forms
-toggleForm.addEventListener('click', function () {
-    if (signupForm.style.display === 'block') {
-        loginForm.style.display = 'block';
-        signupForm.style.display = 'none';
-        cardToggleBtn.textContent = 'Switch to Login';
-    }
-    else {
-        loginForm.style.display = 'none';
-        signupForm.style.display = 'block';
-        cardToggleBtn.textContent = 'Switch to Sign Up';
+toggleForm.addEventListener("click", function () {
+    if (signupForm.style.display === "block") {
+        loginForm.style.display = "block";
+        signupForm.style.display = "none";
+        cardToggleBtn.textContent = "Switch to Login";
+    } else {
+        loginForm.style.display = "none";
+        signupForm.style.display = "block";
+        cardToggleBtn.textContent = "Switch to Sign Up";
     }
 });
 
 // Handle form submission
-loginForm.addEventListener('submit', function (e) {
+loginForm.addEventListener("submit", function (e) {
     e.preventDefault();
-    // Perform login validation here
-    alert('Login successful!');
-    // Redirect to index.html dashboard
+
+    // Sign In validation
+    // !!! HARSHA GET ME MY EMAIL AND PASSWORD FOR SIGN IN I NEED IT HERE
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            alert("Login successful!");
+            location.replace("https://localhost:5000/index.html");
+        })
+        .catch((error) => {
+            alert(`Error code: ${error.code}\nError message: ${error.message}`);
+            location.reload()
+        });
+    
+    //
 });
 
-signupForm.addEventListener('submit', function (e) {
+signupForm.addEventListener("submit", function (e) {
     e.preventDefault();
     // Perform signup validation here
-    const username2Input = document.querySelector('#username2');
-    const email2Input = document.querySelector('#email');
-    const password2Input = document.querySelector('#password2');
+    // !!! HARSHA GET ME MY EMAIL AND PASSWORD FOR SIGN UP I NEED IT HERE
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            alert("Login successful!");
+            location.replace("https://localhost:5000/index.html");
+        })
+        .catch((error) => {
+            alert(`Error code: ${error.code}\nError message: ${error.message}`);
+            location.reload()
+        });
+    //
 
-    username2Input.addEventListener('input', function () {
+    const username2Input = document.querySelector("#username2");
+    const email2Input = document.querySelector("#email");
+    const password2Input = document.querySelector("#password2");
+
+    username2Input.addEventListener("input", function () {
         if (this.validity.valueMissing) {
-            this.setCustomValidity('Please enter your username');
+            this.setCustomValidity("Please enter your username");
         } else {
-            this.setCustomValidity('');
+            this.setCustomValidity("");
         }
     });
 
-    password2Input.addEventListener('input', function () {
+    password2Input.addEventListener("input", function () {
         if (this.validity.valueMissing) {
-            this.setCustomValidity('Please enter your password');
+            this.setCustomValidity("Please enter your password");
         } else {
-            this.setCustomValidity('');
+            this.setCustomValidity("");
         }
     });
-    email2Input.addEventListener('input', function () {
+    email2Input.addEventListener("input", function () {
         if (this.validity.valueMissing) {
-            this.setCustomValidity('Please enter your email');
+            this.setCustomValidity("Please enter your email");
         } else if (this.validity.typeMismatch) {
-            this.setCustomValidity('Please enter a valid email address');
+            this.setCustomValidity("Please enter a valid email address");
         } else {
-            this.setCustomValidity('');
+            this.setCustomValidity("");
         }
     });
-    alert('Sign up successful!');
+    alert("Sign up successful!");
+
     // Show login form after successful signup
-    const emailInput = document.querySelector('#email');
-    const passwordInput = document.querySelector('#password');
+    const emailInput = document.querySelector("#email");
+    const passwordInput = document.querySelector("#password");
 
-
-    emailInput.addEventListener('input', function () {
+    emailInput.addEventListener("input", function () {
         if (this.validity.valueMissing) {
-            this.setCustomValidity('Please enter your email');
+            this.setCustomValidity("Please enter your email");
         } else {
-            this.setCustomValidity('');
+            this.setCustomValidity("");
         }
     });
 
-    passwordInput.addEventListener('input', function () {
+    passwordInput.addEventListener("input", function () {
         if (this.validity.valueMissing) {
-            this.setCustomValidity('Please choose a password');
+            this.setCustomValidity("Please choose a password");
         } else {
-            this.setCustomValidity('');
+            this.setCustomValidity("");
         }
     });
-    loginForm.style.display = 'block';
-    signupForm.style.display = 'none';
-    cardToggleBtn.textContent = 'Switch to Login';
+    loginForm.style.display = "block";
+    signupForm.style.display = "none";
+    cardToggleBtn.textContent = "Switch to Login";
 });
 
 // Add floating label effect to input fields
-const formGroups = document.querySelectorAll('.form-group');
-formGroups.forEach(group => {
-    const input = group.querySelector('input');
-    input.addEventListener('focus', function () {
-        group.classList.add('focused');
+const formGroups = document.querySelectorAll(".form-group");
+formGroups.forEach((group) => {
+    const input = group.querySelector("input");
+    input.addEventListener("focus", function () {
+        group.classList.add("focused");
     });
-    input.addEventListener('blur', function () {
-        if (input.value === '') {
-            group.classList.remove('focused');
+    input.addEventListener("blur", function () {
+        if (input.value === "") {
+            group.classList.remove("focused");
         }
     });
 });
-
